463e782b30452acdcb21faa499ddca5aeb755983
Peter DeMartini <thepeterdemartini@gmail.com>
2017-03-16 14:24:52 -0700

    v3.0.0 use octoblu/node base image

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -28,7 +28,7 @@ module.exports = {
     filename: 'js/[name].js',
     chunkFilename: 'js/[name].[chunkhash:8].chunk.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
-    publicPath: process.env.CDN + '/v' + PKG_VERSION
+    publicPath: '/'
   },
   resolve: {
     extensions: ['', '.js', '.jsx', '.json'],
